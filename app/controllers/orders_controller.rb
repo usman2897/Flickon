c7bf8b3570872562b32bc5660ce357c53fb357a4
usman2897 <musman2897@gmail.com>
2017-05-23 13:38:41 +0530

    Added Dasboard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  layout 'standard'
   before_action :set_order, only: [:show, :edit, :update, :destroy]
  
   # GET /orders
@@ -6,16 +7,24 @@ class OrdersController < ApplicationController
   @item1 = Item.new
   def index
     current_account_user
-    @orders = Order.where( "user_id = ?", @current_user.user_id)
+    if account_user_signed_in?
+      @orders = Order.where( "user_id = ?", @current_user.user_id)
+    else
+      redirect_to users_login_url
+    end
   end
 
   # GET /orders/1
   # GET /orders/1.json
   def show
+    current_account_user
     @order = Order.find_by(order_id: params[:id])
+    if account_user_signed_in? &&  @current_user.user_id != @order.user_id
+      redirect_to orders_url
+    end
   end
 
-  # GET /orders/new
+  # GET /orders/new?item=1
   def new
     if account_user_signed_in? != true
       redirect_to users_login_url
